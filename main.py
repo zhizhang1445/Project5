f5c983ca7cf6c17488ac43c5de8cd34f4ff90d07
Zhi Zhang <zhi.zhang144515@gmail.com>
2023-07-05 15:35:33 +0200

    Please create your own folder/branch for testing

---
+++ main.py
@@ -0,0 +1,14 @@
+import numpy as np
+import scipy
+import scipy.sparse as sparse
+import matplotlib.pyplot as plt
+
+#This is the main file to run scripts, please only put finished 
+#code here so we don't create merge conflicts
+from methodsBallisticDeposition import *
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main
---
+++ methodsBallisticDeposition.py
@@ -0,0 +1,6 @@
+import numpy as np
+import scipy
+import scipy.sparse as sparse
+import matplotlib.pyplot as plt
+
+#this is the main module with all the methods we need, updatesteps go there
