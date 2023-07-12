af78fe1160d047a7a5d8dc98b376d2b4512858fa
Zhi Zhang <zhi.zhang144515@gmail.com>
2023-07-12 22:10:27 +0200

    I'm not touching this tomorrow

--- main.py
+++ main.py
@@ -13,84 +13,107 @@ def main(params):
     height = params["height"]
     width = params["x_dom"]
     length = params["y_dom"]
-    num_particles = params["num_particles"]
     r = params["r"]
-    t = n_ptcls = n_updates = 0
+    foldername = params["foldername"]
+    filename = params["filename"]
+    t = n_ptcls = n_snapshot = 0 
+    space_time = []
+    propensities_time = []
 
     space = np.zeros((width, length, height), dtype=int) #actual simulation space
     max_height = np.zeros((width, length), dtype=int) #occupation/height at each site
-    propensities = np.ones((width, length), dtype = float)
-
-    adata = ad.AnnData()
-
-    for key, value in params.items():
-        setattr(adata, key, value)
-
-    adata.write("./SimResults/result.h5ad")
+    propensities = np.ones((width, length), dtype = float) #probability of droping at each coordinate
+    taus = np.random.exponential(1/propensities) #purtiaty times generated from exponential distribution
 
     while(t<params["t_max"]):
         try:
-            tau = 1/np.sum(propensities) * np.log(1/np.random.random())
+            index_chosen, tau_min = choose_from_tau(taus)
+            t += tau_min
+            space, max_height = add_point(index_chosen, space, max_height)
+            propensities_new = update_propensities(propensities, index_chosen, params, dt = tau_min)
 
-            index_flat, probability = get_nonzero_propensities(propensities)
-            choice = np.random.choice(len(index_flat), p = probability)
-            x_ind, y_ind = index_flat[choice]
+            if params["all_resample"]:
+                with np.errstate(divide="ignore"):
+                    taus = np.random.exponential(1/propensities_new)
+                    taus[np.isnan(taus)] = np.inf
+            else:
+                with np.errstate(all='ignore'):
+                    taus = (np.divide(propensities, propensities_new)*(taus - tau_min))
+                    taus[np.isnan(taus)] = np.inf
 
-            near_neighbors = get_nearest_neighbors(propensities, index_flat[choice])
-            # Find the highest position in the selected column
-            height_surrondings = [max_height[neighbor[0], neighbor[1]] for neighbor in near_neighbors]
+                taus[index_chosen[0], index_chosen[1]
+                ] = np.random.exponential(1/propensities_new[index_chosen[0], 
+                                                            index_chosen[1]])
 
-            highest_pos = max([max(height_surrondings), max_height[x_ind, y_ind]+1])
-
-            # Deposit the particle at the next position above the highest position
-            space[x_ind, y_ind, highest_pos] = 1
-            max_height[x_ind, y_ind] = highest_pos
+            propensities = propensities_new
             
-            t += tau
-
             if (
-                t > n_updates*params["dt_update_time"]
+                t > n_snapshot*params["dt_snapshot"]
                 ) or (
-                    num_particles%params["n_ptcl_update"] == 0
+                    n_ptcls%params["n_ptcl_snapshot"] == 0
                     ):
                 
-                plot_surface(space[:, 0, :].transpose(),show = False, title=f"Ballistic Deposition time: {t}",  save= True, name = f"frame_{n_updates}.png")
-                propensities = update_propensities(propensities, index_flat[choice], params)
-                n_updates += 1
+                propensities_time.append(propensities)
+                space_time.append(space)
+
+                if params["gif"]:
+                    surface = space[:, 0, :].transpose() if (params["y_dom"] == 1) else max_height
+
+                    plot_surface(surface,
+                                 title = f"r: {r} | time: {t}",  
+                                 save = params["gif"],
+                                 show = False,
+                                 name = f"./{foldername}/frame_{n_snapshot}")
+                n_snapshot += 1
             n_ptcls += 1
 
-        except KeyboardInterrupt or IndexError:
-            print(f"Stopped at time: {t}| N_Ptcls: {n_ptcls}| N_updates: {n_updates}")
+        except IndexError:
+            if (np.max(max_height) == params["height"]-1):
+                print(f"Fully Occupied at time: {t}| N_Ptcls: {n_ptcls}| N_snapshots: {n_snapshot}")
+            else:
+                raise IndexError("This IndexError is NOT expected")
+            break
+
+        except KeyboardInterrupt:
+            print(f"Manually Stopped at time: {t}| N_Ptcls: {n_ptcls}| N_snapshots: {n_snapshot}")
             break
     else:
-        print(f"Stopped at time: {t}| N_Ptcls: {n_ptcls}| N_updates: {n_updates}")
+        print(f"Stopped at time: {t}| N_Ptcls: {n_ptcls}| N_snapshots: {n_snapshot}")
 
     frames = []
     n_updates = 0
-    foldername = "SimResults"
-
-    while(True):
+    while(params["gif"]):
         try:
             image = imageio.v2.imread(f'./{foldername}/frame_{n_updates}.png')
             os.remove(f'./{foldername}/frame_{n_updates}.png')
             frames.append(image)
             n_updates += 1
-        except FileNotFoundError: 
+        except FileNotFoundError:
+            imageio.mimsave(f'./{foldername}/{filename}.gif', 
+                        frames, fps = 30)
             break
-
-    imageio.mimsave(f'./{foldername}/Animation_r_{params["r"]}.gif', 
-                    frames, fps = 30)
+    return space_time
 
 if __name__ == "__main__":
     params = {
-        "height":           400,
-        "x_dom":            200,
-        "y_dom":              1,
-        "num_particles":   2000,
-        "t_max":            100,
-        "r":               0.75,
-        "dt_update_time":     1,          
-        "n_ptcl_update": np.inf,          
+    "height":               400,
+    "x_dom":                400,
+    "y_dom":                  1,
+    "t_max":                100,
+    "r":                    0.3,
+    "dt_snapshot":            1,          
+    "n_ptcl_snapshot":   np.inf,
+    "gif":                 True,
+    "all_resample":       False,
+    "foldername":  "SimResults",
+    "filename":        "result2",
     }
-    main(params)
-    
+
+    space_time, _ = main(params)
+    space_time = [space.flatten() for space in space_time if isinstance(space, np.ndarray) ]
+    adata = ad.AnnData(np.array(space_time).squeeze())
+
+    adata.uns.update(params)
+    foldername = params["foldername"]
+    filename = params["filename"]
+    adata.write(f"./{foldername}/{filename}.h5ad")
