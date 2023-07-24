b4db3c527b7d8f70ee128b05f944215c7d9a0e5b
tommyocari <138686791+tommyocari@users.noreply.github.com>
2023-07-24 19:27:29 +0200

    Create simulation_Tommaso.py
    Create new folder to store simulations results

---
+++ Tommaso/simulation_Tommaso.py
@@ -0,0 +1,25 @@
+import numpy as np
+import sys
+sys.path.append('../')
+from main_Tommaso import main
+
+
+r_0 = float(sys.argv[1])
+L = int(sys.argv[2])
+seed = int(sys.argv[3])
+
+params = {
+    "height":            np.inf,
+    "dom":                    L,
+    "ndim":                   1,
+    "t_max":           10000000,
+    "r_0":                  r_0,
+    "tau":                    1,
+    "dt_snapshot":            1,          
+    "n_ptcl_snapshot":   np.inf,
+    "foldername":  "SimResults",
+    "filename":        "result",
+    "seed":                seed,
+}
+
+main(params)
